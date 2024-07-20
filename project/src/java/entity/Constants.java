f99fd6dfa91572c30d68b64246386e187c503f8a
Dao Manh Tuan <92378795+tuandz1@users.noreply.github.com>
2024-07-20 21:08:11 +0700

    tuan
    dmhe170730

--- project/src/java/entity/Constants.java
+++ project/src/java/entity/Constants.java
@@ -10,11 +10,11 @@ package entity;
  */
 public class Constants {
 
-    public static String GOOGLE_CLIENT_ID = "495179221908-hrqrs564g8ts8i4   oqjagiq8k4uvl1a52.apps.googleusercontent.com";
-    public static String GOOGLE_CLIENT_SECRET = "GOCSPX-DxMxD9vT6BChWLd    9FVIMnQpjeMhp";
-    public static String GOOGLE_REDIRECT_URI = "http:  //local  host:9999/   WatchProject/login-google";
-     public static String GOOGLE_LINK_GET_TOKEN = "https:/   /accounts.   google.com/o/oauth2/token";
-    public static String GOOGLE_LINK_GET_USER_INFO = "https:   //www.goo    gleapis.com/oauth2/v1/userinfo?access_token=";
+    public static String GOOGLE_CLIENT_ID = "495179221908-hrqrs564g8ts8i4oqjagiq8k4uvl1a52.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/WatchProject/login-google";
+     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
     public static String GOOGLE_GRANT_TYPE = "authorization_code";
 
 }
